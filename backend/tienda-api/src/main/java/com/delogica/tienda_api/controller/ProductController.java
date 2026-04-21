5d4d1844a158e8a8a27c8868aa39406a11af292a
Diego Forteza Benito <06590020Z@oficina.delogica.com>
2026-04-21 13:10:24 +0200

    fix: solve authentication flyway bug in pom

---
+++ backend/tienda-api/src/main/java/com/delogica/tienda_api/controller/ProductController.java
@@ -0,0 +1,6 @@
+package com.delogica.tienda_api.controller;
+
+public class ProductController 
+{
+    
+}
--- backend/tienda-api/src/main/java/com/delogica/tienda_api/service/CustomerService.java
+++ backend/tienda-api/src/main/java/com/delogica/tienda_api/service/CustomerService.java
@@ -1,6 +1,5 @@
 package com.delogica.tienda_api.service;
 
-import java.lang.StackWalker.Option;
 import java.util.List;
 import java.util.Optional;
 
